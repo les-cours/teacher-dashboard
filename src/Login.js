56629d9f143f2db73f8c29112657334be7df02d7
chouaib <chouaibe708@gmai.com>
2024-06-14 22:47:02 +0100

    fix

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ function Login() {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "http://172.19.160.1:3004/teacher-login",
+        "http://127.0.0.1:3004/teacher-login",
         {
           username,
           password,
